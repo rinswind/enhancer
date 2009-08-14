cb2277538c54e8d0a93940a682731641177027f5
rinswind <rinswind@77a649a6-878a-11de-a332-fd40ed1fd0be>
2009-08-14 10:59:06 +0000

    - Applied Apache 2.0 license

--- enhancer.lib/src/main/java/enhancer/Generator.java
+++ enhancer.lib/src/main/java/enhancer/Generator.java
@@ -1,5 +1,26 @@
+/**
+ * Copyright (C) 2009 Todor Boev
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package enhancer;
 
+/**
+ * Dynamically generates enhancement classes. Each implementation performs a
+ * single specific type of e enhancement (e.g. AOP advice) based on proxying.
+ * 
+ * @author rinsvind@gmail.com (Todor Boev)
+ */
 public interface Generator {
   /**
    * Tests if the passed class is used by the implementation of the
@@ -24,7 +45,8 @@ public interface Generator {
    * @param context source of classes and resources to drive code generation.
    *          The context must provide all classes needed to link the input
    *          class.
-   * @return
+   * @return the raw bytes of of a class. Some class loader must call
+   *         <code>defineClass()</code> on these to introduce them into the JVM.
    */
   byte[] generate(String inputClass, String outputClass, ClassLoader context);
 }
--- examples.importer.broken/src/main/java/enhancer/examples/importer/broken/internal/Activator.java
+++ examples.importer.broken/src/main/java/enhancer/examples/importer/broken/internal/Activator.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (C) 2009 Todor Boev
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package enhancer.examples.importer.broken.internal;
 
 import java.lang.reflect.Method;
@@ -24,13 +39,13 @@ public class Activator implements BundleActivator {
      * Must reach with reflection to call this bundle's class loader
      * defineClass() - it is a protected method.
      */
-    Method meth = ClassLoader.class.getDeclaredMethod("defineClass", String.class, byte[].class,
-        int.class, int.class);
-    meth.setAccessible(true);
+    Method defineClass = ClassLoader.class.getDeclaredMethod(
+        "defineClass", String.class, byte[].class, int.class, int.class);
+    defineClass.setAccessible(true);
 
     /* Make a proxy class */
     @SuppressWarnings("unchecked")
-    Class<Goodbye> proxyClass = (Class<Goodbye>) meth.invoke(local, outputName, raw, 0, raw.length);
+    Class<Goodbye> proxyClass = (Class<Goodbye>) defineClass.invoke(local, outputName, raw, 0, raw.length);
 
     /* Drive it */
     ServiceReference ref = bc.getServiceReference(Goodbye.class.getName());
